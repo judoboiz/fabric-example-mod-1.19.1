0bf778f3b307e866c30260fdd0777a6cbb95b19b
judoboiz <86206504+StijepoDev@users.noreply.github.com>
2022-08-03 14:42:02 +0200

    bruh

--- src/main/java/net/judoboiz/tutorialmod/block/ModBlock.java
+++ src/main/java/net/judoboiz/tutorialmod/block/ModBlock.java
@@ -2,6 +2,8 @@ package net.judoboiz.tutorialmod.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.judoboiz.tutorialmod.block.custom.JumpyBlock;
+import net.judoboiz.tutorialmod.block.custom.MumLampBlock;
 import net.judoboiz.tutorialmod.item.ModItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -29,6 +31,16 @@ public class ModBlock {
             new Block(FabricBlockSettings.of(Material.METAL)
                     .strength(3f)
                     .requiresTool()
+            ), ModItemGroup.MUM);
+
+    public static final Block MUM_LAMP= registerBlock("mum_lamp",
+            new MumLampBlock(FabricBlockSettings.of(Material.GLASS)
+                    .luminance(state ->state.get(MumLampBlock.LIT)? 15: 0)
+            ), ModItemGroup.MUM);
+
+    public static final Block JUMPY_BLOCK = registerBlock("jumpy_block",
+            new JumpyBlock(FabricBlockSettings.of(Material.STONE)
+                    .strength(2f, 3f)
             ), ModItemGroup.MUM);
     private static Block registerBlock(String name, Block block, ItemGroup group){
         registerBlockItem(name, block, group);
--- src/main/java/net/judoboiz/tutorialmod/item/custom/EightBallItem.java
+++ src/main/java/net/judoboiz/tutorialmod/item/custom/EightBallItem.java
@@ -1,13 +1,20 @@
 package net.judoboiz.tutorialmod.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class EightBallItem extends Item {
     public EightBallItem(Settings settings) {
@@ -24,7 +31,21 @@ public class EightBallItem extends Item {
     }
     private void  outputRandomNumber(PlayerEntity player){
         player.sendMessage(Text.literal("Your number is"+ getRandomNumber()));
+
     }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()){
+            tooltip.add(Text.literal("Right click to get random number").formatted(Formatting.YELLOW));
+
+        }else {
+            tooltip.add(Text.literal("Press shift for more info").formatted(Formatting.YELLOW));
+        }
+
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+
     private int getRandomNumber(){
         return Random.createLocal().nextInt(10);
     }
