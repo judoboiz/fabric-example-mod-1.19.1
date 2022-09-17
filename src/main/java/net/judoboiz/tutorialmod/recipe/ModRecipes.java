2686bed63bdfc3e0de796e3a3a2c30a951c6276d
judoboiz <86206504+StijepoDev@users.noreply.github.com>
2022-09-17 11:22:08 +0200

    gem infusing station recipe

---
+++ src/main/java/net/judoboiz/tutorialmod/recipe/ModRecipes.java
@@ -0,0 +1,14 @@
+package net.judoboiz.tutorialmod.recipe;
+
+import net.judoboiz.tutorialmod.TutorialMod;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModRecipes {
+    public static void registerRecipes() {
+        Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(TutorialMod.MOD_ID, GemInfusingRecipe.Serializer.ID),
+                GemInfusingRecipe.Serializer.INSTANCE);
+        Registry.register(Registry.RECIPE_TYPE, new Identifier(TutorialMod.MOD_ID, GemInfusingRecipe.Type.ID),
+                GemInfusingRecipe.Type.INSTANCE);
+    }
+}
